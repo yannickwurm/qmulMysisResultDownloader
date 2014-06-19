79cb38b24c1fe37b770d0372a0f82380e34797ac
Yannick Wurm <y.wurm@qmul.ac.uk>
2014-06-19 11:58:47 +0100

    initial commit
    Signed-off-by: Yannick Wurm <y.wurm@qmul.ac.uk>

---
+++ config-example.rb
@@ -0,0 +1,21 @@
+# need to replace all XXXXX   
+
+#college user id & password
+$id       = 'btwXXXXXXXXXXXX'
+$password = 'XXXXXXXXXXXXXXXX'
+# need advisor as it appear on dropdown list. 
+$advisor  = 'Yannick Wurm'
+
+## for sending emails need relevant info - only tested with gmail
+$smtp_server = "smtp.gmail.com"
+$mail_login =  'XXXXXXXXXXXXXXXXXXX@gmail.com'
+$mail_password = 'XXXXXXXXXXXXXXXXXXXX'
+
+# extras for the email                                                                                                                                                                                                                                                
+$mail_from   = 'XXXXXXXXXXXXXXXXX@qmul.ac.uk'
+$mail_intro  = "your results for this year are as follows:\n"
+$mail_outro  = "\nEnjoy the summer & let me know if any questions,\n Yannick\n\n" +
+               "p.s: this is a robot email - reply to my normal qmul email if you want a reply!!\n" +
+               "p.p.s: Unfortunately, we have been explicitly told that we are not allowed to share percentages - only the letter grades." +
+               "The percentages will be visible to you on MySis when everything is (I think around July 8th? or so?)"
+
